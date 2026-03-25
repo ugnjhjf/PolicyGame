c28a15df4b525927a16936c2aea2a40a2ef89154
Rokidna G <128015241+ugnjhjf@users.noreply.github.com>
2026-03-25 11:47:12 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,8 @@ try {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',          
+  basePath: '/PolicyGame',   
   eslint: {
     ignoreDuringBuilds: true,
   },
